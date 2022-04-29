22553f04c2f3fb98a5f4ba93c01c5e60b81ad435
Paul Rowe <prowe@sourceallies.com>
2022-04-28 19:41:00 -0500

    can now watch stuff

--- react-frontend/src/GameInterface.tsx
+++ react-frontend/src/GameInterface.tsx
@@ -1,63 +1,63 @@
-import { gql, useQuery, useSubscription } from "@apollo/client";
+import { useQuery, useSubscription } from "@apollo/client";
 import { useState } from "react";
 import AbilityButton from "./AbilityButton";
 import CharacterStatusDisplay from "./CharacterStatusDisplay";
-import { GetCurrentPlayerDocument, JoinGameMutation, WatchAreaDocument } from "./generated/graphql";
+import { AreaCharacterFragmentDoc, GetCurrentPlayerDocument, GetCurrentPlayerQuery, WatchAreaDocument } from "./generated/graphql";
 
 export interface GameInterfaceProps{
     playerId: string;
 }
 
-// function useWatchArea() {
-//     useSubscription(WatchAreaDocument, {
-//         variables: {
-//             areaId: player.location.id
-//         },
-//         onSubscriptionData: ({subscriptionData, client}) => {
-//             const cache = client.cache;
-//             cache.modify({
-//                 id: cache.identify(player.location),
-//                 fields: {
-//                     charactersPresent: (existingCharacters = [], {readField}) => {
-//                         const newCharacterRef = cache.writeFragment({
-//                             data: subscriptionData.data?.watchArea.gameCharacter,
-//                             fragment: gql`
-//                                 fragment NewCharacter on GameCharacter {
-//                                     id
-//                                     name
-//                                 }
-//                             `
-//                         });
+function useWatchArea (area?: GetCurrentPlayerQuery['player']['location']) {
+    useSubscription(WatchAreaDocument, {
+        skip: !area,
+        variables: {
+            areaId: area?.id ?? ''
+        },
+        onSubscriptionData: ({subscriptionData, client}) => {
+            if (!area) {
+                throw new Error('area is null');
+            }
+            const newCharacter = subscriptionData.data?.watchArea.gameCharacter;
+            const cache = client.cache;
+            cache.modify({
+                id: cache.identify(area),
+                fields: {
+                    charactersPresent: (existingCharacters = [], {readField}) => {
+                        const newCharacterRef = cache.writeFragment({
+                            data: newCharacter,
+                            fragment: AreaCharacterFragmentDoc
+                        });
                           
-//                         // Quick safety check - if the new comment is already
-//                         // present in the cache, we don't need to add it again.
-//                         // if (existingCommentRefs.some(
-//                         //     ref => readField('id', ref) === newComment.id
-//                         // )) {
-//                         //     return existingCommentRefs;
-//                         // }
+                        // Quick safety check - if the new comment is already
+                        // present in the cache, we don't need to add it again.
+                        // if (existingCommentRefs.some(
+                        //     ref => readField('id', ref) === newComment.id
+                        // )) {
+                        //     return existingCommentRefs;
+                        // }
 
-//                         console.log('Modified cache for new data', newCharacterRef);
-//                         return [...existingCharacters, newCharacterRef];
-//                     }
-//                 }
-//             });
-//         }
-//     });
-// }
+                        console.log('Modified cache for new data', newCharacterRef);
+                        return [...existingCharacters, newCharacterRef];
+                    }
+                }
+            });
+        }
+    });
+}
 
 export default function GameInterface({playerId}: GameInterfaceProps) {
     const [targetId, setTargetId] = useState<string | null>(null);
     const {data, loading, error} = useQuery(GetCurrentPlayerDocument, {
         variables: {
             playerId
-        }
+        },
     });
-    if (loading || !data?.player) {
+    const player = data?.player;
+    useWatchArea(player?.location);
+    if (loading || !player) {
         return <progress />;
     }
-
-    const player = data.player;
 
     return (
         <div>
